cb2443a41635d09c2d7284b9bd9aa0887c335519
Michael Wells <info@sygnal.com>
2024-10-20 10:06:27 +0000

    POAC page built

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 (() => {
   // src/version.ts
-  var VERSION = "0.1.12";
+  var VERSION = "0.1.13";
 
   // node_modules/flatpickr/dist/esm/types/options.js
   var HOOKS = [
@@ -11687,12 +11687,83 @@
     }
   };
 
+  // src/page/poac.ts
+  var PoacPage = class {
+    constructor() {
+    }
+    setup() {
+    }
+    exec() {
+      var SimplybookWidget;
+      console.log("exec");
+      (function(w2, d2, s3, i2) {
+        const script = d2.createElement(s3);
+        script.async = true;
+        script.src = "//widget.simplybook.me/v2/widget/widget.js";
+        script.onload = function() {
+          const widgetConstructor = w2.SimplybookWidget;
+          if (typeof widgetConstructor !== "function") {
+            console.error("SimplybookWidget is not defined as a function. The script may not have loaded properly.");
+            return;
+          }
+          const widget = new widgetConstructor({
+            "widget_type": "button",
+            "url": "https://luxradiology.simplybook.me",
+            "theme": "air",
+            "theme_settings": {
+              "timeline_modern_display": "as_slots",
+              "timeline_hide_unavailable": "1",
+              "hide_past_days": "0",
+              "timeline_show_end_time": "0",
+              "sb_base_color": "#d75481",
+              "display_item_mode": "list",
+              "booking_nav_bg_color": "#58b9db",
+              "body_bg_color": "#f2f2f2",
+              "sb_review_image": "",
+              "dark_font_color": "#333333",
+              "light_font_color": "#ffffff",
+              "btn_color_1": "#d75481",
+              "sb_company_label_color": "#ffffff",
+              "hide_img_mode": "1",
+              "show_sidebar": "1",
+              "sb_busy": "#b3b3b3",
+              "sb_available": "#ffdee8"
+            },
+            "timeline": "modern",
+            "datepicker": "top_calendar",
+            "is_rtl": false,
+            "app_config": {
+              "clear_session": 1,
+              "allow_switch_to_ada": 0,
+              "predefined": {}
+            },
+            "button_title": "Book now",
+            "button_background_color": "#d75481",
+            "button_text_color": "#ffffff",
+            "button_position": "right",
+            "button_position_offset": "55%",
+            "container_id": i2
+          });
+          d2.querySelectorAll('[book="sbm"]').forEach((element) => {
+            element.addEventListener("click", () => {
+              widget.options.app_config.predefined.service = element.getAttribute("book-serviceid");
+              console.log(widget);
+              widget.showPopupFrame("book");
+            });
+          });
+        };
+        d2.head.appendChild(script);
+      })(window, document, "script", "sbw_0kr3c2");
+    }
+  };
+
   // src/routes.ts
   var routeDispatcher = () => {
     var routeDispatcher2 = new RouteDispatcher(Site);
     routeDispatcher2.routes = {
       "/": HomePage,
       "/scan": MaternityScanCalcPage,
+      "/poac": PoacPage,
       "/services/*": ServicesPage,
       "/test/wfu-if": TestWfuIfPage
     };
--- dist/version.js
+++ dist/version.js
@@ -1,6 +1,6 @@
 "use strict";
 (() => {
   // src/version.ts
-  var VERSION = "0.1.12";
+  var VERSION = "0.1.13";
 })();
 //# sourceMappingURL=version.js.map
